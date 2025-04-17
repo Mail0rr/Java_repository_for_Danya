85c438884556fee3084c6bf10e73fd3983bb67eb
Mail0rr <Vvorlovv7799@gmail.com>
2025-04-17 19:13:58 +0300

    gf

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def progress_tracker():
     return render_template('index.html')
 
 
-@app.route('/signup/', methods=['GET'])
+@app.get('/sign-up/')
 def get_signup():
     return render_template('signup.html')
 
 
-@app.post('/signup/')
+@app.post('/sign-up/')
 def post_signup():
     username = request.form.get('username')
     email = request.form.get('email')
@@ -24,12 +24,12 @@ def post_signup():
     return redirect(url_for('get_login'))
 
 
-@app.get('/signin/')
+@app.get('/sign-in/')
 def get_login():
     return render_template('signin.html')
 
 
-@app.post('/signin/')
+@app.post('/sign-in/')
 def post_login():
     username = request.form.get('username')
     email = request.form.get('email')
@@ -43,6 +43,50 @@ def profile():
     return render_template('profile.html')
 
 
+@app.route('/profile-settings/')
+def profile_settings():
+    return render_template('profile-settings.html')
+
+
+@app.route('/update-username/')
+def profile_username():
+    return render_template('profile-username.html')
+
+
+@app.post('/update-username/')
+def update_username():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    return redirect(url_for('profile_settings'))
+
+
+@app.route('/update-email/')
+def profile_email():
+    return render_template('profile-email.html')
+
+
+@app.post('/update-email/')
+def update_email():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return redirect(url_for('profile_settings'))
+
+
+@app.route('/update-password/')
+def profile_password():
+    return render_template('profile-password.html')
+
+
+@app.post('/update-password/')
+def update_password():
+    current_password = request.form.get('current-password')
+    new_password = request.form.get('new-password')
+    confirm_password = request.form.get('confirm-password')
+
+    return redirect(url_for('profile_settings'))
+
+
 @app.route('/verification/')
 def verification():
     return render_template('verification.html')
@@ -50,7 +94,8 @@ def verification():
 
 @app.route('/terms/')
 def get_terms():
-    return render_template('termas.html')
+    return render_template('terms.html')
+
 
 @app.get('/authors/')
 def authors():
